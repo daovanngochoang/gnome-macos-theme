1f53d5914d3cc7ed5b6e578f8762267847ecaf69
Đào Văn Ngọc Hoàng <117742602+andreas-aka-hoangdao@users.noreply.github.com>
2022-11-12 10:31:35 -0500

    Add files via upload

---
+++ workspace-install.sh
@@ -0,0 +1,112 @@
+#!/bin/bash
+
+
+home=$(pwd)
+
+#update dnf
+echo "Update ... "
+echo "-----------------------------------------------------------------------------------------------------------------"
+sudo dnf update
+
+
+echo "Installing custom theme and config work space"
+echo "-----------------------------------------------------------------------------------------------------------------"
+
+#Install alacritty
+sudo dnf install alacritty -y
+
+rm ~/.oh-my-zsh -rf
+rm ~/.zshrc 
+
+
+#install zsh and ohmyzsh
+sudo dnf install zsh 
+wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh
+sh install.sh
+
+exit
+
+#install theme, fonts and  ...
+wget https://github.com/andreas-aka-hoangdao/gnome-macos-theme/releases/download/MacOS-Bigsur-Theme/MacOS-Bigsur-Theme.zip
+wait 
+
+unzip MacOS-Bigsur-Theme.zip
+
+cd MacOS-Bigsur-Theme
+
+rm ~/.oh-my-zsh -rf
+rm ~/.zshrc 
+
+cp -r alacritty/ ~/.config/
+cp -r gtk.css ~/.config/gtk-3.0/
+cp -r .icons/ ~
+cp -r .fonts/ ~
+cp -r .oh-my-zsh/ ~ 
+cp .zshrc ~
+
+zsh
+source ~/.zshrc
+
+p10k configure
+
+echo "Installing MacOS Theme ..."
+echo "-----------------------------------------------------------------------------------------------------------------"
+./theme/WhiteSur-gtk-theme/install.sh
+sudo dnf install gnome-tweaks
+
+
+echo " "
+echo " "
+echo "Configure the Coding Environment ..."
+echo " "
+echo " "
+
+#install brave
+echo "Installing Brave Browser"
+echo "-----------------------------------------------------------------------------------------------------------------"
+sudo dnf install dnf-plugins-core
+
+sudo dnf config-manager --add-repo https://brave-browser-rpm-release.s3.brave.com/x86_64/
+
+sudo rpm --import https://brave-browser-rpm-release.s3.brave.com/brave-core.asc
+
+sudo dnf install brave-browser
+
+
+echo "Installing Coding Environment"
+
+#install docker
+echo "Installing Docker"
+echo "-----------------------------------------------------------------------------------------------------------------"
+
+sudo dnf -y install dnf-plugins-core
+sudo dnf config-manager \
+    --add-repo \
+    https://download.docker.com/linux/fedora/docker-ce.repo
+sudo dnf install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+
+
+#Install donet
+echo "Installing dotnet6.0 and asp.net core 3.1"
+echo "-----------------------------------------------------------------------------------------------------------------"
+sudo dnf install dotnet-sdk-6.0
+sudo dnf install dotnet-sdk-3.1
+
+echo "Installing JetBrains Rider"
+echo "-----------------------------------------------------------------------------------------------------------------"
+firefox https://www.jetbrains.com/rider/download/download-thanks.html?platform=linux https://az764295.vo.msecnd.net/stable/6261075646f055b99068d3688932416f2346dd3b/code-1.73.1-1667967421.el7.x86_64.rpm
+
+
+mkdir ~/.Programs
+#cp ~/Download/JetBrains.Rider-2022.2.4.tar.gz ~/.Programs
+#cd ~/.Programs
+#tar -xvf ~/.Programs/JetBrains.Rider-2022.2.4.tar.gz
+
+#cd ~/Download && sudo dnf install ./code-1.73.1-1667967421.el7.x86_64.rpm
+ rm install.sh MacOS-Bigsur-Theme MacOS-Bigsur-Theme.zip
+
+
+
+
+
+
