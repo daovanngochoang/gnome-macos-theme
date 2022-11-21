46474dd540bd6d75b66d6870aa7de6b51a9edfca
daovanngochoang <hoangdao.dev@gmail.com>
2022-11-21 22:42:41 +0700

    add debian install

--- debian-workspace-install.sh
+++ debian-workspace-install.sh
@@ -22,9 +22,41 @@ cp .imwheelrc ~
 
 sudo gpasswd -a $USER input
 newgrp input
+
 sudo nala install libinput-tools ruby xdotool
 sudo gem install fusuma
 
+mkdir -p ~/.config/fusuma        # create config directory
+#nano ~/.config/fusuma/config.yml # edit config file.
+
+echo """swipe:
+  3:
+    left:
+      command: "xdotool key alt+Right" # History forward
+    right:
+      command: "xdotool key alt+Left" # History back
+    up:
+      command: "xdotool key super" # Activity
+    down:
+      command: "xdotool key super" # Activity
+  4:
+    left:
+      command: "xdotool key ctrl+alt+Down" # Switch to next workspace
+    right:
+      command: "xdotool key ctrl+alt+Up" # Switch to previous workspace
+    up:
+      command: "xdotool key ctrl+alt+Down" # Switch to next workspace
+    down:
+      command: "xdotool key ctrl+alt+Up" # Switch to previous workspace
+pinch:
+  in:
+    command: "xdotool keydown ctrl click 4 keyup ctrl" # Zoom in
+  out:
+    command: "xdotool keydown ctrl click 5 keyup ctrl" # Zoom out
+hold:
+  4:
+    command: "xdotool key super" # Activity
+    """ >> ~/.config/fusuma/config.yml
 
 sudo snap remove --purge firefox
 sudo snap remove --purge snap-store
